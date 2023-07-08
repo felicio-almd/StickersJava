c2c2d89bd641784f8e296370e235a9364036eba9
Felicio Rocha <feliciorar@gmail.com>
2023-07-08 00:31:56 -0300

    modified API

--- sticker-felicio/src/API.java
+++ sticker-felicio/src/API.java
@@ -2,7 +2,10 @@ public enum API {
 
     IMDB_TOP_MOVIES("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json", new ExtratorConteudoIMDB()),
     IMDB_TOP_SERIES("https://imdb-api.com/en/API/Top250TVs/k_x3pev8lm", new ExtratorConteudoIMDB()),
-    NASA("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-01-12&end_date=2023-01-15", new ExtratorConteudoNasa());
+    NASA("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/NASA-APOD.json", new ExtratorConteudoNasa()),
+    LINGUAGENS("http://localhost:8080/linguagens", new ExtratorConteudoLinguas());
+    
+
 
     private String url;
     private ExtratorConteudo extrator;
